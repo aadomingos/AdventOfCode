11e24bdf08e0c2ee66f91feff3ad4bbb4e3de958
Armando <armando.domingos@gmail.com>
2020-12-07 21:46:17 -0800

    Day 2 Advent of Code

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,17 +1,1 @@
 """This is the Advent of Code Day 2 challenge."""
-
-import itertools
-
-# Read file into a list of integers
-with open("/Users/AADAir/GitHub/AdventOfCode//Day1/Day1.txt", "r") as f:
-    numberList = [int(number) for number in f]
-
-# Use itertools to get all the unique two digit combinations in the list
-for combo in itertools.combinations(numberList, 3):
-    # Sum the unique tuple
-    winners = sum(list(combo))
-    # If tuple equals the 2020, multiply the elements for the answer, if not, pass
-    if winners == 2020:
-        print(combo[0]* combo[1])
-    else:
-        pass
