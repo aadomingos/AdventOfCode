5c1cbc02598ef331d7a30d17e62927a1a41eea9a
Armando <armando.domingos@gmail.com>
2020-12-09 07:12:35 -0800

    Day 2 update

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,1 +1,7 @@
 """This is the Advent of Code Day 2 challenge."""
+
+with open("/Users/AADAir/GitHub/AdventOfCode/Day2/Day2.txt", "r") as f:
+    passwordList = f.read().splitlines()
+
+data = [password.split(":") for password in passwordList]
+print(data)
